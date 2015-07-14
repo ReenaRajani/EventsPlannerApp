9388e7f7146b9e5b38efb8641963d8368474d091
Reena <reenahrajani@gmail.com>
2015-07-15 05:37:55 +1000

    remember me token added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email_id: params[:session][:email_id].downcase)
     if user.present? && user.authenticate(params[:session][:password])
       session[:user_id] = user.id #logs in the give user 
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user # redirected to the main page 
 
     else
@@ -13,7 +14,8 @@ class SessionsController < ApplicationController
     end
   end
   def destroy
-     session[:user_id] = nil
+    forget(current_user)
+    session[:user_id] = nil
     @current_user = nil
     redirect_to root_path
 
--- app/models/User.rb
+++ app/models/User.rb
@@ -14,10 +14,34 @@
 
 class User < ActiveRecord::Base
 
+  attr_accessor :remember_token
+
   before_save { self.email_id = email_id.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email_id ,:presence => true, :uniqueness => true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+  VALID_PHONENO_REGEX = /\+?[0-9]{1,3}-?[0-9]{6,12}/
+  validates :phone_no , :presence => true, format: {with: VALID_PHONENO_REGEX}
   has_and_belongs_to_many :events
   has_secure_password  
   
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # remembers a user in the database for persistent sessions 
+  def remember
+    self.remember_token = User.new_token
+    updated_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+   # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
